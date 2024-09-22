561e3b959b9158facf53bca2bacfe305827d017a
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-22 22:28:17 +0600

    selected added to the seat

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 // finding element
 const selectedSeat = document.getElementById('selectedSeat');
 
+const displaySelectedSeat = document.getElementById('displaySelectedSeat');
+
+const availableSeat = document.getElementById('available-seat');
 
 
+const totalSelectedSeat = [];
 function selectedSeatHandler(event){
+
+  event.classList.add('bg-primary');
+  event.classList.add('text-white');
+
+  totalSelectedSeat.push(event.innerText);
+  displaySelectedSeat.innerText = totalSelectedSeat.length;
+
+  const availableSeatNumber = parseInt(availableSeat.innerText);
+  
   
 
   selectedSeat.innerHTML = `
